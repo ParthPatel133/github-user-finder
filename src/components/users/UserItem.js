9e91f15a32da8599812720a83dae32f56c6c94d3
parthpatel1004 <parthpatel1004@gmail.com>
2020-11-06 23:20:32 +0530

    convert stateless class to functional component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
-    
-    render() {
-        const  {login, avtar_url, html_url} = this.props.user;
-        return (
-            <div className='card text-center'>
-                <img
-                    src={avtar_url}
-                    alt=''
-                    className='round-img'
-                    style={{ width : '60px'}} 
-                />
-                <h3>{login}</h3>
-                <div>
-                    <a href={html_url} target='blank' className='btn btn-dark btn-sm my-1'>More</a>
-                </div>
-                username
+
+const UserItem = ({ user: { login, avtar_url, html_url } }) => {
+
+    return (
+        <div className='card text-center'>
+            <img
+                src={avtar_url}
+                alt=''
+                className='round-img'
+                style={{ width: '60px' }}
+            />
+            <h3>{login}</h3>
+            <div>
+                <a href={html_url} target='blank' className='btn btn-dark btn-sm my-1'>More</a>
             </div>
-        )
-    }
+                username
+        </div>
+    )
+}
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
 }
 
 export default UserItem;
