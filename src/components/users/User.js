146abf585d9c5318f946acfa7f91c35871442225
parthpatel1004 <parthpatel1004@gmail.com>
2020-11-07 19:01:32 +0530

    create single user route and data

---
+++ src/components/users/User.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export class User extends Component {
+    componentDidMount(){
+        this.props.getUser(this.props.match.params.login);
+    }
+
+    render() {
+        const {
+            name,
+            avatar_url,
+            location,
+            html_url,
+            login,
+            bio,
+            blog,
+            followers,
+            following,
+            public_repos,
+            public_gists,
+            hireable
+        } = this.props.user;
+
+        const {loading} = this.props;
+
+        return (
+            <div>
+                {login}
+            </div>
+        )
+    }
+}
+
+export default User
